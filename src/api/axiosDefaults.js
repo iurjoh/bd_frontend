d65ae6c92105ab28bdabe55ef9a9deae7b581220
Iuri Johansson <iuri.johansson@gmail.com>
2023-08-28 18:03:52 +0000

    Use react rout to creat my routs and install axios to start my authentication process

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://iurjoh-baby-daily-backend-api-1674476236b8.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
