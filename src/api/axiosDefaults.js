815e4fbcab0eb7463f5eec203ffdf3b59b569b44
Iuri Johansson <iuri.johansson@gmail.com>
2023-09-01 15:25:24 +0000

    Update heroku bd backend api address

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://iurjoh-baby-daily-backend-api-1674476236b8.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
